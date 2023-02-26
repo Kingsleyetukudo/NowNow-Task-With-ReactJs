e6ed08f71e1793c414306dbbb63c48faf33612d3
Kingsley Etukudo <kingsleyj90@gmail.com>
2023-02-26 10:25:09 +0100

    just created the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Hello React js with vite</h1>
+    </div>
+  );
+}
+
+export default App;
